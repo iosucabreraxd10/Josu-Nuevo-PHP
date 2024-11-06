e21978f83c5b7118c1f50b7c62c9613babd67a9a
iosucabreraxd10 <jcabrerasa22dw@ikzubirimanteo.com>
2024-11-06 09:20:32 +0100

    carpetas de sobra borradas

--- Peliculas/bloque_2/fromularios/ejer1/get/ejer1.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$numero1 = $_GET['numero1'];
-$numero2 = $_GET['numero2'];
-$suma = $numero1 + $numero2;
-
-echo "La suma de $numero1 y $numero2 es = $suma";
-
-?>
--- Peliculas/bloque_2/fromularios/ejer1/post/ejer1.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$numero1 = $_POST['numero1'];
-$numero2 = $_POST['numero2'];
-$suma = $numero1 + $numero2;
-
-echo "La suma de $numero1 y $numero2 es = $suma";
-
-?>
